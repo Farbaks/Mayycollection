9c4b2c73e6d94d77af775c19e092cb5f6447e7e6
Farbaks <faroukbakre@yahoo.com>
2020-01-08 09:17:50 +0100

    First commit to mayy collection

---
+++ src/template/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+import '../style/Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+
+    </div>
+  );
+}
+
+export default Header;
