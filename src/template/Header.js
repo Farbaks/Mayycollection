68f4cc956ff6ef510e4322bdb7135d129ad09a90
Farbaks <faroukbakre@yahoo.com>
2020-01-08 10:39:17 +0100

    header ui changes

--- src/template/Header.js
+++ src/template/Header.js
@@ -6,12 +6,31 @@ function Header() {
   return (
     <div className="header">
       <div className="header-top">
-        <div className="header-logo">
-        <img src={process.env.PUBLIC_URL+'/images/logo.png'} alt="logo" />
+        <div className="header-top-left">
+          <div className="header-logo">
+            <img src={process.env.PUBLIC_URL+'/images/logo.png'} alt="logo" />
+          </div>
+          <div className="header-category">
+            <a>WOMEN</a>
+            <a>MEN</a>
+            <a>PLACE CUSTOM ORDER</a>
+          </div>
         </div>
-        <div className="header-category">
-          <a>WOMEN</a>
-          <a>MEN</a>
+        
+        <div className="header-search">
+          <input placeholder="Search for items, brands and categories"></input>
+          <button>
+            <img src={process.env.PUBLIC_URL+'/images/searchbutton.svg'} alt="searchbutton" />
+          </button>
+        </div>
+        <div className="header-buttons">
+          <div className="button-icon">
+            <img src={process.env.PUBLIC_URL+'/images/user2.svg'} alt="user" />
+          </div>
+          <div className="button-icon">
+            <img src={process.env.PUBLIC_URL+'/images/shoppingbag.svg'} alt="shoppingbag" />
+            <span>0</span>
+          </div>
         </div>
       </div>
       <div className="header-bottom">
