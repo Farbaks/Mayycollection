ecfb2ea59c8fb24179f30acdc25d9a89ebd18445
Farbaks <faroukbakre@yahoo.com>
2020-01-08 09:45:49 +0100

    location of images was changed
    the image folder was moved from src to public

--- src/template/Header.js
+++ src/template/Header.js
@@ -1,11 +1,22 @@
 import React from 'react';
 // import logo from './logo.svg';
-import '../style/Header.css';
+import '../assets/style/Header.css';
 
 function Header() {
   return (
     <div className="header">
+      <div className="header-top">
+        <div className="header-logo">
+        <img src={process.env.PUBLIC_URL+'/images/logo.png'} alt="logo" />
+        </div>
+        <div className="header-category">
+          <a>WOMEN</a>
+          <a>MEN</a>
+        </div>
+      </div>
+      <div className="header-bottom">
 
+      </div>
     </div>
   );
 }
